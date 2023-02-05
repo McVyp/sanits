0e2c8b677793b161e186df7021adc7f98dc48aee
McVyp <93302780+McVyp@users.noreply.github.com>
2023-02-05 22:28:46 +0545

    sanity-backend

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,10 @@
 import {defineCliConfig} from 'sanity/cli'
 
+const projectId = process.env.NEXT_PUBIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBIC_SANITY_DATASET!;
 export default defineCliConfig({
   api: {
-    projectId: 'js4e4u0o',
-    dataset: 'production'
+    projectId,
+    dataset
   }
 })
